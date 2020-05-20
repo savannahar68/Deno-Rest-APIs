90eacb556ba6227bdcb10ae78c36d553fdb236b9
Savan Nahar <savannahar68@gmail.com>
2020-05-20 23:03:21 +0530

    CRUD rest functions implementation done

--- restApis/controllers/product.ts
+++ restApis/controllers/product.ts
@@ -1,4 +1,5 @@
 import { Product } from "../types.ts";
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
 let products: Product[] = [
   {
@@ -31,4 +32,96 @@ const getProducts = ({ response }: { response: any }) => {
   };
 };
 
-export { getProducts };
+// @desc : Get a single products
+// @route  GET /api/v1/products/:id
+
+const getProduct = (
+  { params, response }: { params: { id: string }; response: any },
+) => {
+  const product: Product | undefined = products.find((p) => p.id === params.id);
+  if (product) {
+    response.status = 200;
+    response.body = {
+      succss: true,
+      data: product,
+    };
+  } else {
+    response.status = 400;
+    response.body = {
+      succss: false,
+      msg: "No product found",
+    };
+  }
+};
+
+// @desc : Add product
+// @route  POST /api/v1/products
+
+const addProduct = async (
+  { request, response }: { request: any; response: any },
+) => {
+  const body = await request.body();
+  if (!request.hasBody) {
+    response.status = 400;
+    response.body = {
+      success: false,
+      msg: "No Data",
+    };
+  } else {
+    const product: Product = body.value;
+    product.id = v4.generate();
+    products.push(product);
+    response.status = 201;
+    response.body = {
+      success: true,
+      data: product,
+    };
+  }
+};
+
+const updateProduct = async (
+  { params, request, response }: {
+    params: { id: string };
+    request: any;
+    response: any;
+  },
+) => {
+  const product: Product | undefined = products.find((p) => p.id === params.id);
+
+  if (product) {
+    const body = await request.body();
+
+    const updateData: { name?: string; description?: string; price?: number } =
+      body.value;
+
+    products = products.map((p) =>
+      p.id === params.id ? { ...p, ...updateData } : p
+    );
+
+    response.status = 200;
+    response.body = {
+      success: true,
+      data: products,
+    };
+  } else {
+    response.status = 404;
+    response.body = {
+      success: false,
+      msg: "No product found",
+    };
+  }
+};
+
+// @desc    Delete product
+// @route   DELETE /api/v1/products/:id
+const deleteProduct = (
+  { params, response }: { params: { id: string }; response: any },
+) => {
+  products = products.filter((p) => p.id !== params.id);
+  response.body = {
+    success: true,
+    msg: "Product removed",
+  };
+};
+
+export { getProducts, getProduct, addProduct, updateProduct, deleteProduct };
--- restApis/routes.ts
+++ restApis/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import { getProducts } from "./controllers/product.ts";
+import {
+  getProducts,
+  getProduct,
+  updateProduct,
+  addProduct,
+  deleteProduct,
+} from "./controllers/product.ts";
 const router = new Router();
 
-router.get(
-  "api/v1/products",
-  getProducts,
-);
-
+router.get("/api/v1/products", getProducts)
+  .get("/api/v1/products/:id", getProduct)
+  .post("/api/v1/products", addProduct)
+  .put("/api/v1/products/:id", updateProduct)
+  .delete("/api/v1/products/:id", deleteProduct);
 export default router;
